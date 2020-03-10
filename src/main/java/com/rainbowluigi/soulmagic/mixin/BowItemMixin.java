2b42758548c3bda5e4a7e9a89c72aedda33a6234
Tim <rainbowluigiguy@gmail.com>
2020-03-10 14:20:57 -0400

    Added ability to have the bow detect if the player has a soul quiver

--- src/main/java/com/rainbowluigi/soulmagic/mixin/BowItemMixin.java
+++ src/main/java/com/rainbowluigi/soulmagic/mixin/BowItemMixin.java
@@ -4,20 +4,37 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+import com.rainbowluigi.soulmagic.item.ModItems;
+import com.rainbowluigi.soulmagic.util.ItemHelper;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.BowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
 
 @Mixin(BowItem.class)
 public class BowItemMixin {
 
-	@ModifyVariable(method = "onStoppedUsing", at = @At(value = "STORE"))
-	public boolean hasSoulQuiver(boolean base) {
-		System.out.println("onStoppedUsing");
+	@ModifyVariable(method = "onStoppedUsing", name = {"boolean_1"}, at = @At("STORE"))
+	public boolean hasSoulQuiver(boolean base, ItemStack itemStack_1, World world_1, LivingEntity livingEntity_1, int int_1) {
+		//System.out.println("onStoppedUsing " + base);
+		return base || ItemHelper.findItem((PlayerEntity) livingEntity_1, ModItems.SOUL_QUIVER) != null;
+	}
+	
+	@ModifyVariable(method = "onStoppedUsing", name = {"projectileEntity_1"}, at = @At("STORE"))
+	public ProjectileEntity getSoulQuiverArrow(ProjectileEntity base, ItemStack itemStack_1, World world_1, LivingEntity livingEntity_1, int int_1) {
+		if(ItemHelper.findItem((PlayerEntity) livingEntity_1, ModItems.SOUL_QUIVER) != null) {
+			//System.out.println("getSoulQuiverArrow " + base);
+			return base;
+		}
 		return base;
 	}
 	
-	@ModifyVariable(method = "use", at = @At(value = "STORE"))
-	public boolean hasSoulQuiverOnUse(boolean base) {
-		System.out.println("use");
-		return base;
+	@ModifyVariable(method = "use", name = "boolean_1", at = @At(value = "STORE"))
+	public boolean hasSoulQuiverOnUse(boolean base, World world_1, PlayerEntity playerEntity_1, Hand hand_1) {
+		return base || ItemHelper.findItem(playerEntity_1, ModItems.SOUL_QUIVER) != null;
 	}
 }
