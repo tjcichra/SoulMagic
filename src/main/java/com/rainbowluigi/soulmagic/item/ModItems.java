4b5d1def3094ac2cda5bf4386b69b357e9ec8c7d
Tim <tjcichra@yahoo.com>
2020-06-04 02:11:40 -0400

    Added base item for guide book

--- src/main/java/com/rainbowluigi/soulmagic/item/ModItems.java
+++ src/main/java/com/rainbowluigi/soulmagic/item/ModItems.java
@@ -28,6 +28,7 @@ public class ModItems {
     public static final Item UNIVERSE_RING = new UniverseRingItem(new Item.Settings().maxCount(1).group(SoulMagic.ITEM_GROUP));
     public static final Item VACCUM = new VacuumItem(new Item.Settings().maxCount(1).group(SoulMagic.ITEM_GROUP));
     public static final Item MAGICAL_BALL_OF_YARN = new MagicalBallOfYarnItem(new Item.Settings().maxCount(1).group(SoulMagic.ITEM_GROUP));
+    public static final Item SOUL_MAGIC_BOOK = new SoulMagicBookItem(new Item.Settings().maxCount(1).group(SoulMagic.ITEM_GROUP));
     
     public static final Item LIGHT_SOUL_POWDER = new Item(new Item.Settings().group(SoulMagic.ITEM_GROUP));
     public static final Item DARK_SOUL_POWDER = new Item(new Item.Settings().group(SoulMagic.ITEM_GROUP));
@@ -64,6 +65,7 @@ public class ModItems {
         registerItem(UNIVERSE_RING, "universe_ring");
         registerItem(VACCUM, "vacuum");
         registerItem(MAGICAL_BALL_OF_YARN, "magical_ball_of_yarn");
+        registerItem(SOUL_MAGIC_BOOK, "soul_magic_book");
         
         registerItem(LIGHT_SOUL_POWDER, "light_soul_powder");
         registerItem(DARK_SOUL_POWDER, "dark_soul_powder");
---
+++ src/main/java/com/rainbowluigi/soulmagic/item/SoulMagicBookItem.java
@@ -0,0 +1,10 @@
+package com.rainbowluigi.soulmagic.item;
+
+import net.minecraft.item.Item;
+
+public class SoulMagicBookItem extends Item {
+
+    public SoulMagicBookItem(Settings settings) {
+        super(settings);
+    }
+}
