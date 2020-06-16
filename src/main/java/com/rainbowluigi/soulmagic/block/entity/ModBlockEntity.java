8f4ab2ec6199bd10485d0715f1caad38a24dbb6b
Tim <tjcichra@yahoo.com>
2020-06-16 19:29:00 -0400

    Added build parameter to BlockEntityType builder and tab whitespace

--- src/main/java/com/rainbowluigi/soulmagic/block/entity/ModBlockEntity.java
+++ src/main/java/com/rainbowluigi/soulmagic/block/entity/ModBlockEntity.java
@@ -4,22 +4,24 @@ import com.rainbowluigi.soulmagic.block.ModBlocks;
 import com.rainbowluigi.soulmagic.util.Reference;
 
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.datafixer.TypeReferences;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlockEntity {
-    
-    public static final BlockEntityType<SoulInfuserBlockEntity> SOUL_INFUSER = BlockEntityType.Builder.create(SoulInfuserBlockEntity::new, ModBlocks.SOUL_INFUSER).build(null);
-    public static final BlockEntityType<SoulSeparatorBlockEntity> SOUL_SEPARATOR = BlockEntityType.Builder.create(SoulSeparatorBlockEntity::new, ModBlocks.SOUL_SEPARATOR).build(null);
-    public static final BlockEntityType<SoulCacheBlockEntity> SOUL_CACHE = BlockEntityType.Builder.create(SoulCacheBlockEntity::new, ModBlocks.SOUL_CACHE).build(null);
+	
+	public static final BlockEntityType<SoulInfuserBlockEntity> SOUL_INFUSER = BlockEntityType.Builder.create(SoulInfuserBlockEntity::new, ModBlocks.SOUL_INFUSER).build(Util.method_29187(TypeReferences.BLOCK_ENTITY, "soul_infuser"));
+	public static final BlockEntityType<SoulSeparatorBlockEntity> SOUL_SEPARATOR = BlockEntityType.Builder.create(SoulSeparatorBlockEntity::new, ModBlocks.SOUL_SEPARATOR).build(Util.method_29187(TypeReferences.BLOCK_ENTITY, "soul_separator"));
+	public static final BlockEntityType<SoulCacheBlockEntity> SOUL_CACHE = BlockEntityType.Builder.create(SoulCacheBlockEntity::new, ModBlocks.SOUL_CACHE).build(Util.method_29187(TypeReferences.BLOCK_ENTITY, "soul_cache"));
 
-    public static void registerBlockEntityTypes() {
-    	registerBlockEntityType(SOUL_INFUSER, "soul_infuser");
-    	registerBlockEntityType(SOUL_SEPARATOR, "soul_separator");
-    	registerBlockEntityType(SOUL_CACHE, "soul_cache");
-    }
+	public static void registerBlockEntityTypes() {
+		registerBlockEntityType(SOUL_INFUSER, "soul_infuser");
+		registerBlockEntityType(SOUL_SEPARATOR, "soul_separator");
+		registerBlockEntityType(SOUL_CACHE, "soul_cache");
+	}
 
-    private static void registerBlockEntityType(BlockEntityType<?> blockEntityType, String name) {
-        Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier(Reference.MOD_ID, name), blockEntityType);
-    }
+	private static void registerBlockEntityType(BlockEntityType<?> blockEntityType, String name) {
+		Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier(Reference.MOD_ID, name), blockEntityType);
+	}
 }
